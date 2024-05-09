d307673566d659c0704417d33b5fcdc4bcc3b90d
ldhpop123 <dab060719@gmail.com>
2024-05-09 22:19:36 +0900

    20240509 백준 풀이

---
+++ 10869.py
@@ -0,0 +1,8 @@
+A, B = [int(i) for i in input().split()]
+
+if (1 <= A) and (B <= 10000) :
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(round(A/B))
+    print(A%B)
---
+++ 18108.py
@@ -0,0 +1,4 @@
+year = int(input())
+
+if 1000 <= year <= 3000:
+    print(year-543)
