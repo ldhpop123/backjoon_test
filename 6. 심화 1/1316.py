93abaac2c044cbcf50d38ee09864eb0c45649240
이동휘 <ldhpop123@idonghwiui-MacBookPro.local>
2024-07-02 13:55:20 +0900

    20240702

--- 6. 심화 1/1316.py
+++ 6. 심화 1/1316.py
@@ -1,17 +1,17 @@
 num_li = [input() for i in range(int(input()))]
+values = []
 count = 0
 
 for element in num_li:
     for set_ele in set(element):
-        value = False
-        print(set(element))
         target_index = [index for index, value in enumerate(element) if value == set_ele]
-        if all(target_index[i] == target_index[i-1] + 1 for i in range(1, len(target_index))):
-            print(target_index[0:])
-            value = True
+        if len(target_index) == 1:
+            values.append(True)
         else:
-            value = False
-    if value == True:
+            for value in (target_index[i] == target_index[i-1] + 1 for i in range(1, len(target_index))):
+                values.append(value)
+    if all(values):
         count += 1
+    values = []
 print(count)
 
