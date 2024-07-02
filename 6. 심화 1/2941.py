2c89ec2aef12775b256cef5a3980e02e5034fa40
이동휘 <ldhpop123@idonghwiui-MacBookPro.local>
2024-07-02 09:28:03 +0900

    20240702

--- 6. 심화 1/2941.py
+++ 6. 심화 1/2941.py
@@ -1,41 +1,15 @@
 string = input()
-list_string = []
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+croatia = ['dz=', 'c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
 count = 0
 
 
-list_string = list(string)
-
-while True:
-    print(list_string, '출발')
-    print(string, '출발')
-    for croa in croatia: # 크로아티아에서 i로 하나씩 가져옴
-        if croa in string: # 만약 string 안에 i가 있다면 실행
-            targets = []
-            for target_range in range(len(string)):
-                if string[target_range] == croa[0]:
-                    targets.append(target_range)
-            for target in targets:
-                if len(croa) == 2:
-                    if string[target+1] == croa[1]:
-                        count += 1
-                        for del_index in range(target, target+2):
-                            list_string[del_index] = ' '
-                elif croa == 'dz=':
-                    if string[target+1] == croa[1] and string[target+2] == croa[2]:
-                        count += 1
-                        for del_index in range(target, target+3):
-                            list_string[del_index] = ' '
-    string = ''
-    for i in list_string:
-        string += i
-    for space in list_string:
-        if space != ' ':
+if len(string) <= 100:
+    for repeat in range(len(string)):
+        for croa in croatia:
+            if croa in string:
+                string = string[:string.index(croa)] + ' ' + string[string.index(croa)+len(croa):]
+                count += 1
+    for element in string:
+        if (not element == ' ') and (element.isalpha()):
             count += 1
-    print(count, '결과')
-    print(list_string)
-    print(string)
-    if 
-
-
-
+    print(count)
