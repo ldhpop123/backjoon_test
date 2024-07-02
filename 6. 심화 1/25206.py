ea052aca6f8c00863c03b95dbb8381ec8831aa09
이동휘 <ldhpop123@idonghwiui-MacBookPro.local>
2024-07-02 14:36:43 +0900

    20240702

---
+++ 6. 심화 1/25206.py
@@ -0,0 +1,28 @@
+grade_dic = {}
+
+scores = {
+    'A+': 4.5,
+    'A0': 4.0,
+    'B+': 3.5,
+    'B0': 3.0,
+    'C+': 2.5,
+    'C0': 2.0,
+    'D+': 1.5,
+    'D0': 1.0,
+    'F': 0.0
+}
+
+sum_grade_score = 0
+sum_score = 0
+
+for i in range(20):
+    subject, score, grade = input().split()
+    grade_dic[subject] = {'Grade': grade, 'Score': float(score)}
+
+for key, value in grade_dic.items():
+    sum_score += value['Score']
+    
+    if not value['Grade'] == 'P':
+        sum_grade_score += value['Score'] * scores[value['Grade']]
+
+print(sum_grade_score/sum_score)
