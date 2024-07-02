43f47fe77476ab0d20ef66409ef5b4ffe5f7e31c
이동휘 <ldhpop123@idonghwiui-MacBookPro.local>
2024-07-02 11:15:45 +0900

    20240702

--- 6. 심화 1/2941.py
+++ 6. 심화 1/2941.py
@@ -2,14 +2,12 @@ string = input()
 croatia = ['dz=', 'c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
 count = 0
 
+for croa in croatia:
+    while croa in string:
+        string = string[:string.index(croa)] + ' ' + string[string.index(croa)+len(croa):]
+        count += 1
+for element in string:
+    if (not element == ' ' ) and (element.isalpha()):
+        count += 1
 
-if len(string) <= 100:
-    for repeat in range(len(string)):
-        for croa in croatia:
-            if croa in string:
-                string = string[:string.index(croa)] + ' ' + string[string.index(croa)+len(croa):]
-                count += 1
-    for element in string:
-        if (not element == ' ') and (element.isalpha()):
-            count += 1
-    print(count)
+print(count)
