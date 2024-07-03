7a4f30b5cfbea77fdba327699ff3e6377bcd9679
이동휘 <ldhpop123@idonghwiui-MacBookPro.local>
2024-07-03 20:54:22 +0900

    20240703

--- 7. 2차원 배열/2566.py
+++ 7. 2차원 배열/2566.py
@@ -1,12 +1,13 @@
 num_li = [list(map(int, input().split())) for i in range(9)]
+max_num = num_li[0][0]
 
-for p in range(9):
-    row_li = num_li[p]
-    n = len(row_li)
-    for i in range(n):
-        for o in range(0, n-i-1):
-            print(o)
-            if row_li[o] < row_li[o+1]:
-                row_li[o], row_li[o+1] = row_li[o+1], row_li[o]
-    print(row_li)
-print(num_li)
+for row_li in num_li:
+    for element in row_li:
+        max_num = max_num if max_num > element else element
+print(max_num)
+
+for row_li in num_li:
+    if max_num in row_li:
+        row_index = row_li.index(max_num)+1
+        col_index = num_li.index(row_li)+1
+print(col_index, row_index)
