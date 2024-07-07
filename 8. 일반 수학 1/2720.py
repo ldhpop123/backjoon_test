1b7142fa44a62405fc6c011736e0e16d6f55947c
이동휘 <ldhpop123@idonghwcBookPro.localdomain>
2024-07-07 22:26:09 +0900

    20240707

--- 8. 일반 수학 1/2720.py
+++ 8. 일반 수학 1/2720.py
@@ -1,13 +1,12 @@
 num_li = []
-repeat = int(input)
-for i in range(repeat):
+for i in range(int(input())):
     num_li.append(int(input()))
 
 for num in num_li:
     result = { 25: 0, 10: 0, 5: 0, 1: 0 }
     for key, value in result.items():
         while num >= key:
-            num - key
+            num -= key
             result[key] += 1
     for key, value in result.items():
         print(value, end = ' ')
